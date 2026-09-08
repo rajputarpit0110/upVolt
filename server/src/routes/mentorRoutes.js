fea54fca334f03974606b303f8f3a5eb50cb41f0
Divya Narayan Varshney <manishdnv@gmail.com>
2026-09-09 00:54:56 +0530

    Update mentorRoutes.js

--- server/src/routes/mentorRoutes.js
+++ server/src/routes/mentorRoutes.js
@@ -2,6 +2,7 @@ import { Hono } from 'hono';
 import {
   getMentors,
   createMentor,
+  updateMentor,
   deleteMentor
 } from '../controllers/mentorController.js';
 import { protect, authorize } from '../middlewares/auth.js';
@@ -12,8 +13,9 @@ const router = new Hono();
 // Public: view active mentors
 router.get('/', adaptHandler(getMentors));
 
-// Protected: Only Admin and Master Admin can add or remove mentors
+// Protected: Only Admin and Master Admin can add, edit, or remove mentors
 router.post('/', protect, authorize('admin', 'master_admin'), adaptHandler(createMentor));
+router.put('/:id', protect, authorize('admin', 'master_admin'), adaptHandler(updateMentor));
 router.delete('/:id', protect, authorize('admin', 'master_admin'), adaptHandler(deleteMentor));
 
 export default router;
